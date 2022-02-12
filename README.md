07144ad287179597c4c8e25c3f49deef11184b6f
Dork <62394594+dorian-K@users.noreply.github.com>
2022-02-12 16:51:20 +0100

    Add play command to spotify switcher, rewrite args logic and supply cookie via file

--- README.md
+++ README.md
@@ -3,5 +3,7 @@ Automation Tools
 
 Collection of personal projects to automate my daily life
 
+The code may or may not be documented, browse at your own caution!
+
 - [Spotify Switcher](SpotifySwitcher/) - Switching between spotify devices from anywhere via a python script
 - [MobileAlarmToHomeAssistant](https://github.com/dorian-K/MobileAlarmToHomeAssistant/) - Triggers a homeassistant webhook once an alarm notification is detected on your phone
--- SpotifySwitcher/spotify_switcher.py
+++ SpotifySwitcher/spotify_switcher.py
@@ -1,20 +1,25 @@
+from genericpath import exists
+from msilib.schema import Error
 import sys
+from tkinter import E
 from requests import post, get, put
 import asyncio
 import json
 from websockets import connect
+import argparse
 
 ###########################
 # insert your sp_dc cookie here
+# you can also store your cookie inside a file named sp_dc.password ! 
 sp_dc = "your cookie goes here!"
 ###########################
 
 # feel free to re-generate this number, it's just a random 40-character hex string
 myDeviceId = "a08ad7ab697dcbc67f2f7cc6c48f0ddd55cebd3e"
-# replace with your local urls
+# replace with your local urls 
+# TODO: automatically get these from the spotify api
 dealer = "gew1-dealer.spotify.com"
 spclient = "gew1-spclient.spotify.com"
-
 ###########################
 
 def grabAccessToken(sp_dc): 
@@ -24,6 +29,7 @@ def grabAccessToken(sp_dc):
     }
     r = get(endpoint, cookies=cookies)
 
+    # TODO: parse via html & json parsers instead of searching for a needle in a haystack
     start = r.text.find('"accessToken":"')
     if start == -1:
         raise ValueError("no access token")
@@ -49,7 +55,6 @@ async def withConnectionId(accessToken, callback):
         print("connection id acquired")
         callback(accessToken, conId)
 
-        # time.sleep(5)
         return conId
     raise ValueError("no connection id")
 
@@ -66,6 +71,17 @@ def transferDevice(accessToken, fromD, to):
     }
     payload = json.dumps(obj)
     x = post("https://"+spclient+"/connect-state/v1/connect/transfer/from/"+fromD+"/to/"+to, payload, headers=headers)
+
+    print(x.status_code)
+
+def sendPlayerCommand(accessToken, fromD, toD, command):
+    headers = {
+        "Authorization": "Bearer "+accessToken,
+        "content-type": "application/json",
+        "user-agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/97.0.4692.99 Safari/537.36",
+    }
+    payload = json.dumps(command)
+    x = post("https://"+spclient+"/connect-state/v1/player/command/from/"+fromD+"/to/"+toD, payload, headers=headers)
 
     print(x.status_code)
 
@@ -104,7 +120,9 @@ def registerDevice(accessToken, connectionId):
     payload = json.dumps(obj)
     x = post("https://"+spclient+"/track-playback/v1/devices", payload, headers=headers)
 
-    print(x.status_code)
+    print(x.status_code)#
+
+    return myDeviceId
 
 def listAllDevices(accessToken, connectionId):
     headers = {
@@ -131,9 +149,61 @@ def listAllDevices(accessToken, connectionId):
 
     return re
 
-def doWork(targetDevice):
 
-    def cb(accessToken, connectionId):
+async def runPlayPlaylistOn(args):
+    print("Retrieving token...")
+    tok = grabAccessToken(sp_dc)
+
+    def withConn(accessToken, connectionId):
+        myDev = registerDevice(accessToken, connectionId)
+        devs = listAllDevices(accessToken, connectionId)
+        active = devs["active_device_id"]
+        print("Active device:", active)
+
+        allDevices = devs["devices"]
+        targetDeviceActivated = False
+        targetDeviceId = ""
+        for dev in allDevices.keys():
+            d = allDevices[dev]
+            if d["name"] == args.device:
+                if dev == active:
+                    print("Target device is already active!")
+                    targetDeviceActivated = True
+                    targetDeviceId = dev
+                    break
+                transferDevice(accessToken, active, dev)
+                targetDeviceActivated = True
+                targetDeviceId = dev
+                
+        if targetDeviceActivated == False:
+            print("Target device not found!!!")
+            return
+        
+        cmd = {
+            "command": {
+                "context": {
+                    "metadata": {},
+                    "uri": args.uri,
+                    "url": "context://"+args.uri
+                },
+                "endpoint": "play",
+                "options": {
+                    "license": "premium",
+                    "player_options_override": {},
+                    "skip_to": {}
+                }
+            }
+        }
+        sendPlayerCommand(accessToken, myDev, targetDeviceId, cmd)
+
+    await withConnectionId(tok, withConn)
+
+
+async def runSwitch(args):
+    print("Retrieving token...")
+    tok = grabAccessToken(sp_dc)
+
+    def withConn(accessToken, connectionId):
         registerDevice(accessToken, connectionId)
         devs = listAllDevices(accessToken, connectionId)
         active = devs["active_device_id"]
@@ -148,7 +218,7 @@ def doWork(targetDevice):
                 continue # also print all other devices
             if dev == active:
                 print("^^^^^^ active device")
-            if d["name"] == targetDevice:
+            if d["name"] == args.device:
                 if dev == active:
                     print("Target device is already active!")
                     targetDeviceActivated = True
@@ -158,27 +228,37 @@ def doWork(targetDevice):
                 
         if targetDeviceActivated == False:
             print("Target device not found!!!")
-        
-    return cb
 
-async def run():
+    await withConnectionId(tok, withConn)
+
+def makeRunner(func):
+    def runner(args):
+        asyncio.run(func(args))
+
+    return runner
+
+def run():
     global sp_dc
 
-    if len(sys.argv) < 2:
-        print("Usage: <target device name> [sp_dc]")
-        return
-    if len(sp_dc) < 25 and len(sys.argv) < 3:
-        print("sp_dc is not set! Read the manual before using this program!")
-        return
-    
-    if len(sys.argv) >= 3:
-        sp_dc = sys.argv[2]
+    if exists("sp_dc.password"):
+        file = open("sp_dc.password", "r")
+        sp_dc = file.read()
 
-    target = sys.argv[1]
+    parser = argparse.ArgumentParser(description="cli interface for the spotify api")
+    parser.add_argument('--sp_dc', help="Supply the sp_dc cookie here", nargs='?', default=sp_dc, required=(len(sp_dc) < 30))
 
-    print("Retrieving token...")
-    tok = grabAccessToken(sp_dc)
-    await withConnectionId(tok, doWork(target))
+    subparsers = parser.add_subparsers(help='sub-command help')
+    parser_switch = subparsers.add_parser('switch')
+    parser_switch.add_argument('device')
+    parser_switch.set_defaults(func=makeRunner(runSwitch))
+
+    parser_playPlaylistOn = subparsers.add_parser('playPlaylistOn')
+    parser_playPlaylistOn.add_argument('device')
+    parser_playPlaylistOn.add_argument('uri')
+    parser_playPlaylistOn.set_defaults(func=makeRunner(runPlayPlaylistOn))
+
+    args = parser.parse_args()
+    args.func(args)
 
 if __name__ == "__main__":
-    asyncio.run(run())
+    run()
